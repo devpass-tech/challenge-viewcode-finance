9203ce01619d69d118f1ece98ede460467406279
Mariana Maiko <marimaiko@cora.com.br>
2022-03-07 22:51:52 -0300

    Update UserProfileViewController.swift
    lazy variables created

--- solutions/devsprint-renato-sarro-1/FinanceApp/Screens/UserProfile/UserProfileViewController.swift
+++ solutions/devsprint-renato-sarro-1/FinanceApp/Screens/UserProfile/UserProfileViewController.swift
@@ -9,6 +9,37 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 
+    lazy var profileContent: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var profileImage: UIImageView = {
+       let image = UIImageView()
+        image.layer.cornerRadius = 48
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
+    lazy var profileName:UILabel = {
+       let label = UILabel()
+        label.text = "Irma Flores"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    lazy var profileInfo: UILabel = {
+       let label = UILabel()
+        label.text = "Agency 0001 \n Account 123456-7 \n Devpass Bank"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     override func loadView() {
         self.view = UserProfileView()
     }
