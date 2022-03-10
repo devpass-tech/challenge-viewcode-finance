b9a6e77b10f146853eeb5652b126d93d7dddc5cc
Mateus Coelho <mateusnazarioc@gmail.com>
2022-03-10 19:33:54 -0300

    setting up bind of view buttons

--- solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Transfers/TransfersViewController.swift
+++ solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Transfers/TransfersViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class TransfersViewController: UIViewController {
-
+    // MARK: Properties
+    private let transfersView: TransfersView = {
+        let transfersView = TransfersView()
+        return transfersView
+    }()
+    
+    // MARK: Init
     override func loadView() {
-        self.view = TransfersView()
+        self.view = transfersView
+    }
+    
+    override func viewDidLoad() {
+        bindEvents()
+    }
+    
+    // MARK: Methods
+    private func bindEvents() {
+        transfersView.didPressTransferButton = { [weak self] in
+            print("transfer button tapped!")
+        }
+
+        transfersView.didPressChooseContactButton = { [weak self] in
+            print("contact button tapped!")
+        }
     }
 }
