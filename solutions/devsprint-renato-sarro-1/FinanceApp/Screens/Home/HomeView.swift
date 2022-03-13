d7ade7ffcdb24b37d99bdb2b1c4f5f680716a851
Edson Neto <edson_neto1996@hotmail.com>
2022-03-13 19:59:00 -0300

    Created the activity list view on the home view

--- solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Home/HomeView.swift
+++ solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Home/HomeView.swift
@@ -14,23 +14,19 @@ struct HomeViewConfiguration {
 
 final class HomeView: UIView {
 
-    private let listViewCellIdentifier = "ListViewCellIdentifier"
-
     private var activities: [Activity] = []
 
     private lazy var tableView: UITableView = {
 
         let tableView = UITableView(frame: .zero)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.listViewCellIdentifier)
+        tableView.register(ActivityCellView.self, forCellReuseIdentifier: ActivityCellView.identifier)
         tableView.dataSource = self
         return tableView
     }()
 
     init() {
-
         super.init(frame: .zero)
-
         self.setupViews()
     }
 
@@ -48,7 +44,6 @@ final class HomeView: UIView {
 private extension HomeView {
 
     func setupViews() {
-
         self.backgroundColor = .white
 
         self.configureSubviews()
@@ -56,14 +51,12 @@ private extension HomeView {
     }
 
     func configureSubviews() {
-
         self.addSubview(self.tableView)
     }
 
     func configureSubviewsConstraints() {
 
         NSLayoutConstraint.activate([
-
             self.tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             self.tableView.topAnchor.constraint(equalTo: self.topAnchor),
@@ -75,14 +68,12 @@ private extension HomeView {
 extension HomeView: UITableViewDataSource {
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return self.activities.count
     }
 
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.listViewCellIdentifier)!
-        cell.textLabel?.text = self.activities[indexPath.row].name
+        let cell = tableView.dequeueReusableCell(withIdentifier: ActivityCellView.identifier) as! ActivityCellView
+        cell.populate(detail: activities, indexPath: indexPath)
         return cell
     }
 }
