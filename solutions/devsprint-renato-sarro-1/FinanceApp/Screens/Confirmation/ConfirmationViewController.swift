b2b6b450159c4402380b532c481c849a95f87794
Mariana Maiko <marimaiko@cora.com.br>
2022-03-14 11:19:56 -0300

    Implemented life cycle and view configurations

--- solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Confirmation/ConfirmationViewController.swift
+++ solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Confirmation/ConfirmationViewController.swift
@@ -31,13 +31,44 @@ class ConfirmationViewController: UIViewController {
         
         lazy var buttonConfirmation: UIButton = {
             let button = UIButton()
-            button.layer.backgroundColor = UIColor(red: 0, green: 0.478, blue: 1, alpha: 1).cgColor
+            button.layer.name = "Nice!"
+//            button.set
+            button.backgroundColor = .blue
             button.layer.cornerRadius = 14
+            button.translatesAutoresizingMaskIntoConstraints = false
             
             return button
         }()
+    //MARK: Life cycle
+    override func viewDidLoad() {
+//        self.view = ConfirmationView()
+        super.viewDidLoad()
+        configViews()
+        buildViews()
+        setupConstraints()
+    }
     
-    override func loadView() {
-        self.view = ConfirmationView()
+    //MARK: View Configuration
+    func configViews() {
+        view.backgroundColor = .white
+    }
+    
+    func buildViews() {
+        [imageConfirmation, titleConfirmation, buttonConfirmation].forEach(view.addSubview)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageConfirmation.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageConfirmation.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            titleConfirmation.topAnchor.constraint(equalTo: imageConfirmation.bottomAnchor, constant: 8),
+            titleConfirmation.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            buttonConfirmation.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -21),
+            buttonConfirmation.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            buttonConfirmation.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            buttonConfirmation.heightAnchor.constraint(equalToConstant: 56)
+        ])
     }
 }
