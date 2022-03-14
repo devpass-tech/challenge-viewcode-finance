288ffcd81907f973fe9dabaab03dbf860090b2a2
Renato Sarro <cora@MacBook-Pro-de-Cora-5.local>
2022-03-14 10:16:36 -0300

    Implementa tableview pra exibir os dados de perfil.

--- solutions/devsprint-renato-sarro-1/FinanceApp/Screens/UserProfile/UserProfileViewController.swift
+++ solutions/devsprint-renato-sarro-1/FinanceApp/Screens/UserProfile/UserProfileViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
-class UserProfileViewController: UIViewController {
-
+class UserProfileViewController: UIViewController, ViewConfiguration {
+    
+    fileprivate let titleSections: [String] = [
+        "MY ACCOUNT",
+        "GENERAL"
+    ]
+    
     lazy var profileContent: UIStackView = {
-       let stackview = UIStackView()
+        let stackview = UIStackView()
         stackview.axis = .vertical
         stackview.alignment = .center
         stackview.spacing = 8
@@ -31,7 +36,7 @@ class UserProfileViewController: UIViewController {
     }()
     
     lazy var profileName:UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = "Irma Flores"
         label.font = .systemFont(ofSize: 17, weight: .bold)
         label.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
@@ -42,7 +47,7 @@ class UserProfileViewController: UIViewController {
     }()
     
     lazy var profileInfo: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.textAlignment = .center
         label.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
         label.font = .systemFont(ofSize: 15, weight: .regular)
@@ -53,12 +58,28 @@ class UserProfileViewController: UIViewController {
         return label
     }()
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .clear
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UserProfileCell")
+        
+        return tableView
+    }()
+    
     //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        configViews()
-        buildViews()
-        setupConstraints()
+        setupViews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.contentInset = UIEdgeInsets(top: profileContent.frame.height + 32,
+                                              left: 0,
+                                              bottom: 0,
+                                              right: 0)
     }
     
     //MARK: View Configuration
@@ -67,8 +88,8 @@ class UserProfileViewController: UIViewController {
     }
     
     func buildViews() {
-        view.addSubview(profileContent)
-        [profileImage, profileName, profileInfo].forEach(profileContent.addArrangedSubview)
+        view.addSubviews([profileContent, tableView])
+        profileContent.addArrangedSubviews([profileImage, profileName, profileInfo])
     }
     
     func setupConstraints() {
@@ -81,7 +102,35 @@ class UserProfileViewController: UIViewController {
             profileImage.centerXAnchor.constraint(equalTo: profileContent.centerXAnchor),
             profileImage.heightAnchor.constraint(equalToConstant: 100),
             profileImage.widthAnchor.constraint(equalToConstant: 100),
-          
+            
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+}
+
+extension UserProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return titleSections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return titleSections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: "UserProfileCell")
+        
+        cell.textLabel?.text = "Phone"
+        cell.detailTextLabel?.text = "+55 (11) 99999-9999"
+        
+        return cell
     }
 }
