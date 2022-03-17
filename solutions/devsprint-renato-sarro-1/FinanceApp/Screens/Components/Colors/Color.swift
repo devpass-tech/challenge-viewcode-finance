c07dbe2b2fe3c120b9b1b2c03f9f1576871d910b
Renato Sarro <cora@MacBook-Pro-de-Cora-5.local>
2022-03-17 20:00:59 -0300

    Implementa estrutura de color via assets.

--- solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Components/Colors/Color.swift
+++ solutions/devsprint-renato-sarro-1/FinanceApp/Screens/Components/Colors/Color.swift
@@ -13,13 +13,26 @@ enum Color {
     
     var color: UIColor {
         switch self {
-        case .primary: return UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+        case .primary: return getPrimaryColor()
         case .secondary: return UIColor(red: 0.24, green: 0.24, blue: 0.26, alpha: 1.00)
         case .green: return UIColor(red: 0.20, green: 0.78, blue: 0.35, alpha: 1.00)
         case .red: return UIColor(red: 1.00, green: 0.23, blue: 0.19, alpha: 1.00)
         case .purple: return UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1.00)
         case .blue: return UIColor(red: 0.35, green: 0.78, blue: 0.98, alpha: 1.00)
         case .orange: return UIColor(red: 1.00, green: 0.62, blue: 0.04, alpha: 1.00)
+        }
+    }
+    
+    private func getPrimaryColor() -> UIColor {
+        return UIColor.init { (trait) -> UIColor in
+            
+            if #available(iOS 13, *) {
+                return trait.userInterfaceStyle == .dark ?
+                UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00) :
+                UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+            } else {
+                return UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+            }
         }
     }
 }
