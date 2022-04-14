2aa0ec1bb3538edb29b4882707ae15dca62533fe
Leticia de Oliveira Speda <85207486+LeticiaSpeda@users.noreply.github.com>
2022-04-14 19:57:56 -0300

    Feature (TabBarButton): Add tabbar na navigation

--- solutions/devsprint-devpoli-1/FinanceApp/Screens/UserProfile/UserProfileView.swift
+++ solutions/devsprint-devpoli-1/FinanceApp/Screens/UserProfile/UserProfileView.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 class UserProfileView: UIView {
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        backgroundColor = .white
+    }
+    
 }
