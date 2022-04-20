15fab711eb83bc76923bad5a9c3b456e3a590932
Vinicius Rocha <vinicius.workspace@gmail.com>
2022-04-20 20:48:47 -0300

    test: add long title test

--- solutions/devsprint-devpoli-1/FinanceAppTests/Components/ActivityCellViewTests.swift
+++ solutions/devsprint-devpoli-1/FinanceAppTests/Components/ActivityCellViewTests.swift
@@ -12,7 +12,7 @@ import UIKit
 
 class ActivityCellViewTests: XCTestCase {
     
-    private lazy var defaultStateMock: Activity = Activity(name: "Mall", price: 100, time: "8:57 AM")
+    private var defaultStateMock: Activity = Activity(name: "Mall", price: 100, time: "8:57 AM")
     private lazy var sut: ActivityCellView = ActivityCellView()
     
     override func setUp() {
@@ -22,12 +22,15 @@ class ActivityCellViewTests: XCTestCase {
     }
 
     func testDefaultState() {
-        sut.backgroundColor = .white
+        assertSnapshot(matching: sut, as: .image)
+    }
+    
+    func testLongTitleActivityState() {
+        sut.setupActivity(activity: Activity(name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie nisl mi, nec accumsan ex blandit vitae. Aliquam volutpat orci vitae venenatis consectetur. Nullam facilisis lorem eu justo posuere, sed efficitur purus interdum.", price: 100, time: "8:57 AM"))
         assertSnapshot(matching: sut, as: .image)
     }
     
     func testDarkMode() {
-        sut.backgroundColor = .black
         sut.overrideUserInterfaceStyle = .dark
         assertSnapshot(matching: sut, as: .image)
     }
