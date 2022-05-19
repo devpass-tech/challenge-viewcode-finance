6eab7ecb5428068d8aff3c4e5d266a64cb5f2536
Razee <razee.hj@gmail.com>
2022-05-19 17:56:15 +0300

    Implements `updateView` to show the information to the UI

--- solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/Cell/ActivityCellView.swift
+++ solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/Cell/ActivityCellView.swift
@@ -7,19 +7,56 @@
 
 import UIKit
 
+struct ActivityCellConfiguration {
+    let icon: String
+    let name: String
+    let information: String
+}
+
 class ActivityCellView: UITableViewCell {
-    
-    private lazy var image: UIImageView = {
-        let image = UIImageView()
+    private lazy var iconView: UIImageView = {
+        let iconView = UIImageView()
         
-        return image
+        return iconView
     }()
     
     private lazy var name: UILabel = {
         let name = UILabel()
+        name.font = UIFont(name: "SFProText-Semibold", size: 15)
         
         return name
     }()
     
-
+    private lazy var information: UILabel = {
+        let information = UILabel()
+        information.font = UIFont(name: "SFProText-Regular", size: 13)
+        information.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+        
+        return information
+    }()
 }
+
+// MARK: - Update Cell View
+
+extension ActivityCellView {
+    func updateView(with configuration: ActivityCellConfiguration) {
+        self.iconView.image = UIImage(named: configuration.icon)
+        self.name.text = configuration.name
+        self.information.text = configuration.information
+    }
+}
+
+#if DEBUG
+
+import SwiftUI
+
+struct ActivityCellView_Preview: PreviewProvider {
+    static var previews: some View {
+        return SwiftUIPreView { _ in
+            let cell = ActivityCellView()
+            
+            return cell
+        }
+    }
+}
+#endif
