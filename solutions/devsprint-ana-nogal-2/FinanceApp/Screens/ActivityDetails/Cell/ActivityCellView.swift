6e6176bfe55f44e2ee27184e9d71331f668c9d6e
Razee <razee.hj@gmail.com>
2022-05-19 18:31:47 +0300

    Implements configuration and layout for `ActivityCellView`

--- solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/Cell/ActivityCellView.swift
+++ solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/Cell/ActivityCellView.swift
@@ -20,20 +20,38 @@ class ActivityCellView: UITableViewCell {
         return iconView
     }()
     
-    private lazy var name: UILabel = {
-        let name = UILabel()
-        name.font = UIFont(name: "SFProText-Semibold", size: 15)
+    private lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.font = .boldSystemFont(ofSize: 15)
         
-        return name
+        return nameLabel
     }()
     
-    private lazy var information: UILabel = {
-        let information = UILabel()
-        information.font = UIFont(name: "SFProText-Regular", size: 13)
-        information.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+    private lazy var descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.font =  .systemFont(ofSize: 13)
+        descriptionLabel.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
         
-        return information
+        return descriptionLabel
     }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 // MARK: - Update Cell View
@@ -41,8 +59,45 @@ class ActivityCellView: UITableViewCell {
 extension ActivityCellView {
     func updateView(with configuration: ActivityCellConfiguration) {
         self.iconView.image = UIImage(named: configuration.icon)
-        self.name.text = configuration.name
-        self.information.text = configuration.information
+        self.nameLabel.text = configuration.name
+        self.descriptionLabel.text = configuration.information
+    }
+}
+
+// MARK: - Customize User Interface
+
+extension ActivityCellView {
+    private func configure() {
+        configureSubviews()
+        configureSubviewsConstraints()
+    }
+    
+    private func configureSubviews() {
+        self.addSubview(iconView)
+        self.addSubview(nameLabel)
+        self.addSubview(descriptionLabel)
+    }
+    
+    private func configureSubviewsConstraints() {
+        self.iconView.translatesAutoresizingMaskIntoConstraints = false
+        self.nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.iconView.widthAnchor.constraint(equalToConstant: 48),
+            self.iconView.heightAnchor.constraint(equalToConstant: CGFloat(48)),
+            self.iconView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+        ])
+        
+        NSLayoutConstraint.activate([
+            self.nameLabel.topAnchor.constraint(equalTo: self.iconView.topAnchor, constant: 5),
+            self.nameLabel.leadingAnchor.constraint(equalTo: self.iconView.trailingAnchor, constant: 12)
+        ])
+        
+        NSLayoutConstraint.activate([
+            self.descriptionLabel.topAnchor.constraint(equalTo: self.nameLabel.bottomAnchor),
+            self.descriptionLabel.leadingAnchor.constraint(equalTo: self.iconView.trailingAnchor, constant: 12)
+        ])
     }
 }
 
@@ -54,6 +109,8 @@ struct ActivityCellView_Preview: PreviewProvider {
     static var previews: some View {
         return SwiftUIPreView { _ in
             let cell = ActivityCellView()
+            let configuration = ActivityCellConfiguration(icon: "bag.circle.fill", name: "Mall", information: "$100.00 • 8:57 AM")
+            cell.updateView(with: configuration)
             
             return cell
         }
