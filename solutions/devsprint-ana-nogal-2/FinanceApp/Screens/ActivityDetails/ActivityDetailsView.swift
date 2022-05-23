4a92f71ed0010311e13375998644c1c404f49315
Pedro Henrique Leda Araujo Guimaraes <pedro.guimaraes@compasso.com.br>
2022-05-23 12:10:27 -0300

    creating View

--- solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/ActivityDetailsView.swift
+++ solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/ActivityDetailsView.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class ActivityDetailsView: UIView {
-
+    
+    private lazy var activityDetailsImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(imageLiteralResourceName: "bag.circle.fill")
+        image.tintColor = Color.purple.color
+        image.contentMode = .scaleAspectFit
+        image.clipsToBounds = true
+        image.layer.masksToBounds = true
+        return image
+    }()
+    
+    private lazy var titleActivityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mall"
+        label.textColor = Color.primary.color
+        label.textAlignment = .center
+        return label
+    }()
 }
