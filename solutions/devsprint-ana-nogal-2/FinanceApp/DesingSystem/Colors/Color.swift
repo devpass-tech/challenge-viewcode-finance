77f645c94c49007dc617888cfdc2a496e97fc6d5
Pedro Henrique Leda Araujo Guimaraes <pedro.guimaraes@compasso.com.br>
2022-05-24 12:44:46 -0300

    refactoring code

--- solutions/devsprint-ana-nogal-2/FinanceApp/DesingSystem/Colors/Color.swift
+++ solutions/devsprint-ana-nogal-2/FinanceApp/DesingSystem/Colors/Color.swift
@@ -9,17 +9,18 @@ import Foundation
 import UIKit
 
 enum Color {
-    case green, red, purple, blue, orange, primary, secondary, lightGray, lightGray2, lightGreen
+    case green, red, purple, lightBlue, blue, orange, primary, secondary, lightGray, lightGray2, lightGreen
     
     var color: UIColor {
         switch self {
         case .primary: return getPrimaryColor()
-        case .secondary: return UIColor(red: 0.24, green: 0.24, blue: 0.26, alpha: 1.00)
-        case .green: return UIColor(red: 0.20, green: 0.78, blue: 0.35, alpha: 1.00)
-        case .red: return UIColor(red: 1.00, green: 0.23, blue: 0.19, alpha: 1.00)
-        case .purple: return UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1.00)
-        case .blue: return UIColor(red: 0.35, green: 0.78, blue: 0.98, alpha: 1.00)
-        case .orange: return UIColor(red: 1.00, green: 0.62, blue: 0.04, alpha: 1.00)
+        case .secondary: return UIColor(red: 0.24, green: 0.24, blue: 0.26, alpha: 1)
+        case .green: return UIColor(red: 0.20, green: 0.78, blue: 0.35, alpha: 1)
+        case .red: return UIColor(red: 1.00, green: 0.23, blue: 0.19, alpha: 1)
+        case .purple: return UIColor(red: 0.69, green: 0.32, blue: 0.87, alpha: 1)
+        case .lightBlue: return UIColor(red: 0.35, green: 0.78, blue: 0.98, alpha: 1)
+        case .blue: return UIColor(red: 0, green: 0.478, blue: 1, alpha: 1)
+        case .orange: return UIColor(red: 1.00, green: 0.62, blue: 0.04, alpha: 1)
         case .lightGray: return UIColor(red: 0.898, green: 0.898, blue: 0.918, alpha: 1)
         case .lightGray2: return UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
         case .lightGreen: return UIColor(red: 0.204, green: 0.78, blue: 0.349, alpha: 1)
@@ -31,10 +32,10 @@ enum Color {
             
             if #available(iOS 13, *) {
                 return trait.userInterfaceStyle == .dark ?
-                UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00) :
-                UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+                UIColor(red: 1, green: 1, blue: 1, alpha: 1) :
+                UIColor(red: 0, green: 0, blue: 0, alpha: 1)
             } else {
-                return UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
+                return UIColor(red: 0, green: 0, blue: 0, alpha: 1)
             }
         }
     }
--- solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/ActivityDetailsView.swift
+++ solutions/devsprint-ana-nogal-2/FinanceApp/Screens/ActivityDetails/ActivityDetailsView.swift
@@ -58,7 +58,7 @@ class ActivityDetailsView: UIView {
     private lazy var reportButton: UIButton = {
         let button = UIButton()
         button.layer.cornerRadius = 14
-        button.layer.backgroundColor = UIColor(red: 0, green: 0.478, blue: 1, alpha: 1).cgColor
+        button.layer.backgroundColor = Color.blue.color.cgColor
         button.setTitle("Report a issue", for: .normal)
         return button
     }()
@@ -66,10 +66,10 @@ class ActivityDetailsView: UIView {
     init() {
         super.init(frame: .zero)
         configViews()
-        buildViews()
+        buildViewHierarchy()
         setupConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -78,41 +78,41 @@ class ActivityDetailsView: UIView {
         backgroundColor = .white
     }
     
-    func buildViews() {
-        [activityDetailsImage, titleActivityLabel, typeActivityLabel, priceActivityLabel, timeActivityLabel, reportButton].forEach(addSubview)
+    func buildViewHierarchy() {
+        addSubview(activityDetailsImage)
+        addSubview(titleActivityLabel)
+        addSubview(typeActivityLabel)
+        addSubview(priceActivityLabel)
+        addSubview(timeActivityLabel)
+        addSubview(reportButton)
     }
     
     func setupConstraints() {
-            
         [activityDetailsImage, titleActivityLabel, typeActivityLabel, priceActivityLabel, timeActivityLabel, reportButton].forEach { $0.translatesAutoresizingMaskIntoConstraints = false
         }
-
+        
         NSLayoutConstraint.activate([
-            activityDetailsImage.topAnchor.constraint(equalTo: topAnchor, constant: 94),
+            activityDetailsImage.topAnchor.constraint(equalTo: topAnchor, constant: 70),
             activityDetailsImage.centerXAnchor.constraint(equalTo: centerXAnchor),
             activityDetailsImage.heightAnchor.constraint(equalToConstant: 100.0),
             activityDetailsImage.widthAnchor.constraint(equalToConstant: 100.0),
             
-            titleActivityLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 156.0),
             titleActivityLabel.topAnchor.constraint(equalTo: activityDetailsImage.bottomAnchor, constant: 8.0),
-            titleActivityLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -156.0),
+            titleActivityLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             
-            typeActivityLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0.0),
             typeActivityLabel.topAnchor.constraint(equalTo: titleActivityLabel.bottomAnchor, constant: 8.0),
-            typeActivityLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0.0),
+            typeActivityLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             
-            priceActivityLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0.0),
-            priceActivityLabel.topAnchor.constraint(equalTo: typeActivityLabel.bottomAnchor, constant: 192.0),
-            priceActivityLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0.0),
+            priceActivityLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            priceActivityLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
             
-            timeActivityLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0.0),
             timeActivityLabel.topAnchor.constraint(equalTo: priceActivityLabel.bottomAnchor, constant: 8.0),
-            timeActivityLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0.0),
+            timeActivityLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             
             reportButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20.0),
-            reportButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100.5),
             reportButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20.0),
-            reportButton.heightAnchor.constraint(equalToConstant: 56.0)
+            reportButton.heightAnchor.constraint(equalToConstant: 56.0),
+            reportButton.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor, constant: -20)
         ])
     }
 }
