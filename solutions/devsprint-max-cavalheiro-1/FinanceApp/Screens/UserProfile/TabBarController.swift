65a4caea4ddeac16e3b93669f0fdfbcb031115ca
Debora Rodrigues <deboral.p@hotmail.com>
2022-08-16 21:06:21 -0300

    new tabbar

--- solutions/devsprint-max-cavalheiro-1/FinanceApp/Screens/UserProfile/TabBarController.swift
+++ solutions/devsprint-max-cavalheiro-1/FinanceApp/Screens/UserProfile/TabBarController.swift
@@ -9,16 +9,27 @@ import UIKit
 
 class TabBarController: UITabBarController {
     
-    override func viewWillAppear(_ animated: Bool) {
-        
-        let home = HomeViewController()
-        let homeNavigationController = UINavigationController(rootViewController: home)
-        let homeTabBar = UITabBarItem(title: "Home", image: UIImage(named: "house.fill"), tag: 0)
-        homeNavigationController.tabBarItem = homeTabBar
-        
-        let transfers = TransfersViewController()
-        let transferNavigationController = UINavigationController(rootViewController: transfers)
+    private let homeViewController = UINavigationController(rootViewController: HomeViewController())
+    private let transfersViewController = UINavigationController(rootViewController: TransfersViewController())
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+        setupHomeViewController()
+        setupTransferViewController()
+        self.viewControllers = [homeViewController, transfersViewController]
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupTransferViewController() {
         let transferTabBar = UITabBarItem(title: "Transfer", image: UIImage(named: "arrow.up.arrow.down"), tag: 1)
-        homeNavigationController.tabBarItem = transferTabBar
+        transfersViewController.tabBarItem = transferTabBar
+    }
+    
+    private func setupHomeViewController() {
+        let homeTabBar = UITabBarItem(title: "Home", image: UIImage(named: "house.fill"), tag: 0)
+        homeViewController.tabBarItem = homeTabBar
     }
 }
