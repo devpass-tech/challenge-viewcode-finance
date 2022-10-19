f748286e51b3e1acde6411b1d4d7d9486c827f31
Mauricio Maciel <mauriciomaciel@id.uff.br>
2022-10-19 13:36:33 -0300

    snapshot test added - need adjustments

---
+++ solutions/devsprint-caio-santos-7/FinanceAppTests/Screens/Confirmation/ConfirmationViewTests.swift
@@ -0,0 +1,24 @@
+//
+//  ConfirmationViewTests.swift
+//  FinanceAppTests
+//
+//  Created by Mauricio on 19/10/22.
+//
+
+@testable import FinanceApp
+import SnapshotTesting
+import XCTest
+
+final class ConfirmationViewTests: XCTestCase {
+    
+    override class func setUp() {
+//        SnapshotTesting.isRecording = true
+    }
+    
+    func testRenderView() {
+        let component = ConfirmationView()
+        component.backgroundColor = .green
+        assertSnapshot(matching: component, as: .image(size: CGSize(width: UIScreen.main.bounds.width,
+                                                                   height: UIScreen.main.bounds.height)))
+    }
+}
